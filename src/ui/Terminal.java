ac94d2bb9045dd6abdd1b8de2bdbd6f8ada1d937
Methawee A <merimo.mayjiiex@hotmail.com>
2017-03-09 15:09:27 +0700

    A

---
+++ src/ui/Terminal.java
@@ -0,0 +1,50 @@
+package ui;
+
+import java.util.Scanner;
+import game.Game;
+
+public class Terminal {
+
+	private Game game;
+	private Scanner scanner;
+
+	public void run() {
+		// TODO: Write all game logic in here.
+		// If you do it correctly, you don't need to add new
+		// functions into other classes.
+
+		game = new Game();
+		game.start();
+		scanner = new Scanner(System.in);
+		while (!game.isEnd()) {
+			renderBoard(game);
+			System.out.println(game.getCurrentPlayerName() + "'s turn");
+			System.out.print("Row: ");
+			int row = scanner.nextInt();
+			System.out.print("Col: ");
+			int col = scanner.nextInt();
+			game.currentPlayerTakesAction(row - 1, col - 1);
+		}
+		System.out.print(game.getWinnerName());
+	}
+
+	private void renderBoard(Game game) {
+		int size = game.getBoardSize();
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				String s = game.getSymbolOnBoard(row, col);
+				if (s == null) {
+					s = "_";
+				}
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Terminal ui = new Terminal();
+		ui.run();
+	}
+
+}
